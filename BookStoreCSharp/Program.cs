f2c320949c1fe155cb68eb9d6e39d5c7789d8600
RedGuardiaN7 <artiom.str@hotmail.it>
2022-11-22 14:44:15 +0100

    Stampa delle informazione del libro nella console

--- BookStoreCSharp/Program.cs
+++ BookStoreCSharp/Program.cs
@@ -10,11 +10,15 @@ long ISBN = 8833926575;
 
 int pages = 800;
 
+float weight = 500;
+
 float length = 19;
 
 float width = 12.8f;
 
 float depth = 5;
+
+int ReviewsNumber = 208;
 
 float rating = 4.1f;
 
@@ -22,3 +26,19 @@ string KindleAvaible = "Sì";
 
 string FlexCoverAvaible = "Sì";
 
+//Ora che le informazioni sono scritte sottoforma di variabuli, è possibile procedere con la stampa di essi
+
+Console.WriteLine("<<<----------IL LIBRO DI OGGI È: '" + title + "'---------->>>");
+Console.WriteLine();
+Console.WriteLine("Informazioni principali:");
+Console.WriteLine("Autore: " + author);
+Console.WriteLine("ISBN: " + ISBN);
+Console.WriteLine("Numero delle pagine: " + pages + " pagine");
+Console.WriteLine("Peso del ibro: " + weight + "g");
+Console.WriteLine("Dimensioni del libro: " + width + "cm x " + length + "cm x " + depth + "cm");
+Console.WriteLine("-----------------------------------------------------");
+Console.WriteLine("Informazioni Amazon:");
+Console.WriteLine("Numero di recensioni:" + ReviewsNumber + " recensioni");
+Console.WriteLine("Valutazione media: " + rating + " stelle");
+Console.WriteLine("Kindle disponibile? " + KindleAvaible);
+Console.WriteLine("Copertina flessibile disponibile? " + FlexCoverAvaible);
