35db7855d2fac898a9a94c7c10dacbc115d302c1
RedGuardiaN7 <artiom.str@hotmail.it>
2022-11-22 14:19:15 +0100

    Inserite le informazioni del libro "hardcoded"

--- BookStoreCSharp/Program.cs
+++ BookStoreCSharp/Program.cs
@@ -1,2 +1,24 @@
 ﻿//CONSEGNA: Inserire in maniera “hardcoded” tutte le informazioni che troviamo su Amazon di un libro, per poi stamparne una piccola sintesi
 
+//Per prima cosa, viengono aggiunte le informazioni del libro tramite dichiarazione di variabili
+
+string title = "Alan Turing. Storia di un enigma";
+
+string author = "Andrew Hodges";
+
+long ISBN = 8833926575;
+
+int pages = 800;
+
+float length = 19;
+
+float width = 12.8f;
+
+float depth = 5;
+
+float rating = 4.1f;
+
+string KindleAvaible = "Sì";
+
+string FlexCoverAvaible = "Sì";
+
