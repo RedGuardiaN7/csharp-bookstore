e794f4b4d807886c92a4e589ed0c90d85e18869f
RedGuardiaN7 <artiom.str@hotmail.it>
2022-11-22 16:34:02 +0100

    FIne bonus

--- BONUS_Bookstore/Program.cs
+++ BONUS_Bookstore/Program.cs
@@ -1,10 +1,11 @@
 ﻿//BONUS: Le variabili del libro ora devono essere imesse dall'utente tramite console
+using System.Globalization;                            //Viene usato per rendere possibile la trasformazione dell'input in numeri decimali
 
 Console.WriteLine("Qual è il titolo del libro?");
 
 string title = Console.ReadLine();
 
-Console.WriteLine("Chi è del libro?");
+Console.WriteLine("Chi è l'autore del libro?");
 
 string author = Console.ReadLine();
 
@@ -14,23 +15,65 @@ string ImputISBN = Console.ReadLine();
 
 long ISBN = Int64.Parse(ImputISBN);
 
-int pages = 800;
+Console.WriteLine("Quante pagine ha il libro?");
 
-float weight = 500;
+string ImputPages = Console.ReadLine();
 
-float length = 19;
+int pages = Int32.Parse(ImputPages);     
 
-float width = 12.8f;
+Console.WriteLine("Quanti grammi pesa il libro?");
 
-float depth = 5;
+string ImputWeight = Console.ReadLine();
 
-int ReviewsNumber = 208;
+ImputWeight = ImputWeight.Replace(',', '.');
 
-float rating = 4.1f;
+float weight = float.Parse(ImputWeight, CultureInfo.InvariantCulture);
 
-string KindleAvaible = "Sì";
+Console.WriteLine("Quant'è la lunghezza del libro? (cm)");
 
-string FlexCoverAvaible = "Sì";
+string ImputLength = Console.ReadLine();
+
+ImputLength = ImputLength.Replace(',', '.');
+
+float length = float.Parse(ImputLength, CultureInfo.InvariantCulture);
+
+Console.WriteLine("Quant'è la larghezza del libro? (cm)");
+
+string ImputWidth = Console.ReadLine();
+
+ImputWidth = ImputWidth.Replace(',', '.');
+
+float width = float.Parse(ImputLength, CultureInfo.InvariantCulture);
+
+Console.WriteLine("Quant'è la profodnità del libro? (cm)");
+
+string ImputDepth = Console.ReadLine();
+
+ImputDepth = ImputDepth.Replace(',', '.');
+
+float depth = float.Parse(ImputDepth, CultureInfo.InvariantCulture);
+
+Console.WriteLine("Quante recensioni ha il libro?");
+
+string ImputReviews = Console.ReadLine();
+
+int ReviewsNumber = Int32.Parse(ImputReviews);
+
+Console.WriteLine("Qual è la media delle valutazioni del libro? (stelle)");
+
+string ImputRating = Console.ReadLine();
+
+ImputRating = ImputRating.Replace(',', '.');
+
+float rating = float.Parse(ImputRating, CultureInfo.InvariantCulture);
+
+Console.WriteLine("Il libro è disponibile su Kindle?");
+
+string KindleAvaible = Console.ReadLine();
+
+Console.WriteLine("Il libro è disponibile con la copertina flessibile?");
+
+string FlexCoverAvaible = Console.ReadLine();
 
 //Ora che le informazioni sono scritte sotto forma di variabuli, è possibile procedere con la stampa di essi
 
@@ -44,7 +87,7 @@ Console.WriteLine("Peso del ibro: " + weight + "g");
 Console.WriteLine("Dimensioni del libro: " + width + "cm x " + length + "cm x " + depth + "cm");
 Console.WriteLine("-----------------------------------------------------");
 Console.WriteLine("Informazioni Amazon:");
-Console.WriteLine("Numero di recensioni:" + ReviewsNumber + " recensioni");
+Console.WriteLine("Numero di recensioni: " + ReviewsNumber + " recensioni");
 Console.WriteLine("Valutazione media: " + rating + " stelle");
 Console.WriteLine("Kindle disponibile? " + KindleAvaible);
 Console.WriteLine("Copertina flessibile disponibile? " + FlexCoverAvaible);
